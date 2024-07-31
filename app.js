13b782ddce8f93ddff1fea4dfe14a890be82c749
Sykaras <SYKARI@pfizer.com>
2024-07-31 17:05:24 +0300

    work work

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
+app.use(express.json());  // parse body as jason.
 app.use("/api", routes);
 
-app.get('/', (req, res) => {
-    fs.readFile("./members_Array.json", (err, data) => {
-        res.send(data.toString());
-    });
-});
+app.route('/')
+    .get((req, res) => {
+        res.send("Retrieve an existing resource")
+    })
+    .post((req, res) => {
+        res.send("Create a new resource")
+    })
+    .put((req, res) => {
+        res.send("Update an existing resource")
+    })
+    .delete((req, res) => {
+        res.send("Remove an existing resource")
+    })
+
+
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
--- routes.js
+++ routes.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const createMyCustomApi = express.Router();
+const fs = require('fs');
+
+
+
 
 // Define routes
-createMyCustomApi.get('/users', (req, res) => {
-  res.send('List of users');
+createMyCustomApi.get('/getAllMembers', (req, res) => {
+  fs.readFile("./members_Array.json",  (err, data) => {
+    if(err){
+      res.status(500).send(err.message)
+    }
+    res.status(200).send(data.toString());
+  });
 });
 
 
-createMyCustomApi.post('/users', (req, res) => {
-  res.send('Create a new user');
+
+createMyCustomApi.post('/addNewMember', (req, res) => {
+  let data = fs.readFileSync("./members_Array.json"); //diavazoume to array pou exoume hdh
+  let myObject = JSON.parse(data); // to knaoume js object me parsing
+  let newData = req.body; // dilwnoume newData to reqbody apo postman
+    myObject.push(newData); // pusharo sto yparoxn js object to body
+      console.log(myObject);
+    let newArray = JSON.stringify(myObject);
+    fs.writeFile("./members_Array.json", newArray, err => {
+      if(err) throw err;
+      console.log(newArray)
+    });
+    res.sendStatus(200);
 });
 
 createMyCustomApi.put('/users/:id', (req, res) => {
