f67951e72c561c8e7593943e5d319c4d6fbbf6a6
Sykaras <SYKARI@pfizer.com>
2024-08-02 13:21:48 +0300

    paizoume

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const routes = require("./routes")
-const fs = require("fs");
+// const express = require('express');
+// const routes = require("./routes")
+// const fs = require("fs");
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-app.use(express.json());  // parse body as jason.
-// app.use(bodyParser.json());
-app.use("/api", routes);
+// app.use(express.json());  // parse body as jason.
+// // app.use(bodyParser.json());
+// app.use("/api", routes);
 
-app.route('/')
-    .get((req, res) => {
-        res.send("Retrieve an existing resource")
-    })
-    .post((req, res) => {
-        res.send("Create a new resource")
-    })
-    .put((req, res) => {
-        res.send("Update an existing resource")
-    })
-    .delete((req, res) => {
-        res.send("Remove an existing resource")
-    })
-
-
+// app.route('/')
+//     .get((req, res) => {
+//         res.send("Retrieve an existing resource")
+//     })
+//     .post((req, res) => {
+//         res.send("Create a new resource")
+//     })
+//     .put((req, res) => {
+//         res.send("Update an existing resource")
+//     })
+//     .delete((req, res) => {
+//         res.send("Remove an existing resource")
+//     })
 
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
